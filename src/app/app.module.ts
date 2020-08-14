aaf82b448a419555051477419d3f45943a44c2e4
Martin Quintana <martin.quintana@intraway.com>
2020-08-14 10:37:13 -0300

    advance in routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
 import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  declarations: [AppComponent, NopagefoundComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule],
+  exports: [LoginComponent, RegisterComponent],
+})
+export class AuthModule {}
